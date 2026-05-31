d43d2e2a265aca221fb130818c111fa6bb24960a
Dipok Chakraborty <dpkcse@users.noreply.github.com>
2026-06-01 01:04:09 +0600

    Use constant-based Google OAuth login

---
+++ wp-content/plugins/tdl-chauffeur-calendar-sync/uninstall.php
@@ -0,0 +1,6 @@
+<?php
+if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) { exit; }
+delete_option( 'tdl_ccs_settings' );
+delete_option( 'tdl_ccs_google_tokens' );
+delete_option( 'tdl_ccs_logs' );
+delete_transient( 'tdl_ccs_google_refresh_error' );
